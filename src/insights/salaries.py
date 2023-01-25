6bb335febed371040bb3aec9f9afb4578d8af305
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-01-25 19:14:15 -0300

    feat: filter jobs by salary range

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from typing import Union, List, Dict
 from src.insights.jobs import read
 
@@ -19,33 +20,28 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
+    job_copy = deepcopy(job)
     try:
         salary = int(salary)
         for key in ["min_salary", "max_salary"]:
-            job[key] = int(job[key])
-        if job["min_salary"] > job["max_salary"]:
+            job_copy[key] = int(job_copy[key])
+        if job_copy["min_salary"] > job_copy["max_salary"]:
             raise ValueError
     except (ValueError, TypeError, KeyError):
         raise ValueError
-    return salary >= job["min_salary"] and salary <= job["max_salary"]
+    return (salary >= job_copy["min_salary"]
+            and salary <= job_copy["max_salary"])
 
 
 def filter_by_salary_range(
     jobs: List[dict],
     salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+    filtered_jobs = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                filtered_jobs.append(job)
+        except ValueError:
+            pass
+    return filtered_jobs
