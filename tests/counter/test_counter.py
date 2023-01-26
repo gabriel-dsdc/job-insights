99e44a1047e96e3f0c215dd0b04801c4e9dd690c
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-01-26 15:01:34 -0300

    test: tested count_ocurrences case-insensitive

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    words_count = {"PyThOn": 1639, "JaVascRiPt": 122}
+    for word in words_count.keys():
+        counter = count_ocurrences("data/jobs.csv", word)
+        assert counter == words_count[word]
