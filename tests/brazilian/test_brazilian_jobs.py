f074e0505c60d4345e098d06b249624d4d240308
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-01-26 15:38:08 -0300

    test: read_brazilian_file return only english keys

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    result = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    for dict in result:
+        assert all(
+            key in dict.keys()
+            for key in ("title", "salary", "type")
+        )
